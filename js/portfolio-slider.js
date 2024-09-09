7eb1e0c79846f1623509da1986184e39cae60f9c
OleksiiSapronov <oleksiisapronov@outlook.com>
2024-09-09 17:39:48 +0200

    fixed responsive

--- js/portfolio-slider.js
+++ js/portfolio-slider.js
@@ -19,19 +19,26 @@ jQuery(document).ready(function ($) {
       {
         breakpoint: 1440,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 4,
           slidesToScroll: 1,
         },
       },
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 3,
           slidesToScroll: 1,
         },
       },
       {
         breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 450,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
